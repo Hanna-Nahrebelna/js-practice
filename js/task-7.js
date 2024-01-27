7aae160c5fac3048222fdbc8ba2237b83044af8f
Hanna-Nagrehelna <anna.nagrebelna@gmail.com>
2024-01-27 20:28:59 +0200

    task-7 done

---
+++ js/task-7.js
@@ -0,0 +1,23 @@
+/*
+Напиши функцію findSmallesNumber(numbers) для пишуку найменшого числа 
+в масиві, за умови, що числа унікальні (не повторюються).
+*/
+
+
+
+const findSmallesNumber = function (numbers) {
+    let smallestNumber = numbers[0];
+
+    for (const number of numbers) {
+    if (number < smallestNumber) {
+        smallestNumber = number;
+    }
+    }
+    
+    return smallestNumber;
+
+}
+
+console.log(findSmallesNumber([3, 8, 12, -2, 15])); // -2
+console.log(findSmallesNumber([100, 54, 8, 12, 47])); // 8
+console.log(findSmallesNumber([7, 21, 84, 15, 4])); // 4
